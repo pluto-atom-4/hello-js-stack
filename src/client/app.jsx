e86ba1edb160d14037d39d9bd3d060632e1e194b
pluto-atom-4 <pluto-atom-4@users.noreply.github.com>
2018-08-22 12:38:57 -0700

    Chapter05 - Redux, Immutable, and Fetch (#4)
    * adds chapter 05 Immutable and Redux
    
    * adds chapter 05 asynchronous

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,7 +1,20 @@
 // @flow
 
 import React from 'react'
+import HelloButton from './container/hello-button'
+import HelloAsyncButton from './container/hello-async-button'
+import Message from './container/message'
+import MessageAsyc from './container/message-async'
+import { APP_NAME } from '../shared/config'
 
-const App = () => <h1>Hello React!</h1>
+const App = () =>
+// eslint-disable-next-line implicit-arrow-linebreak
+  <div>
+    <h1>{APP_NAME}</h1>
+    <Message />
+    <HelloButton />
+    <MessageAsyc />
+    <HelloAsyncButton />
+  </div>
 
 export default App
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,27 +1,54 @@
+// @flow
+
 import 'babel-polyfill'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
+import { Provider } from 'react-redux'
+import {
+  createStore, combineReducers, applyMiddleware, compose,
+} from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './app'
+import helloReducer from './reducer/hello'
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
+import { isProd } from '../shared/utils'
+
+// eslint-disable-next-line no-underscore-dangle,max-len
+const composeEnhancers = (isProd ? undefined : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+
+const store = createStore(combineReducers({ hello: helloReducer }),
+  composeEnhancers(applyMiddleware(thunkMiddleware)))
 
 const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
 
-const wrapApp = AppComponent => (
-  <AppContainer>
-    <AppComponent />
-  </AppContainer>
-)
+// eslint-disable-next-line no-console
+console.log(rootEl)
 
-ReactDOM.render(wrapApp(App), rootEl)
+const wrapApp = (AppComponent, reduxStore) =>
+// eslint-disable-next-line implicit-arrow-linebreak
+  <Provider store={reduxStore}>
+    <BrowserRouter>
+      <AppContainer>
+        <AppComponent />
+      </AppContainer>
+    </BrowserRouter>
+  </Provider>
 
+if (rootEl !== null) {
+  ReactDOM.render(wrapApp(App, store), rootEl)
+}
+
+// flow-disable-next-line
 if (module.hot) {
-  // flow-disable-next-line
   module.hot.accept('./app', () => {
     // eslint-disable-next-line global-require
     const NextApp = require('./app').default
-    ReactDOM.render(wrapApp(NextApp), rootEl)
+    if (rootEl !== null) {
+      ReactDOM.render(wrapApp(NextApp, store), rootEl)
+    }
   })
 }
