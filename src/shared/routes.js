6345792af1afb7a0e88a8a5db1a2af5f06979bfc
pluto-atom-4 <genko2@hotmail.com>
2018-08-23 08:11:25 -0700

    adds chapter 06 Navigation

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -1,9 +1,9 @@
 // @flow
 
 
-export const HOME_PAGE = '/'
-export const HELLO_PAGE = '/hello'
-export const HELLO_ASYNC_PAGE = '/hello-async'
+export const HOME_PAGE_ROUTE = '/'
+export const HELLO_PAGE_ROUTE = '/hello'
+export const HELLO_ASYNC_PAGE_ROUTE = '/hello-async'
 export const NOT_FOUND_DEMO_PAGE_ROUTE = '/404'
 
 // eslint-disable-next-line import/prefer-default-export
