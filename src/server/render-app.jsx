98cc69c766ef94868034daf81ad00238682096ca
pluto-atom-4 <genko2@hotmail.com>
2018-08-24 11:07:02 -0700

    adds chapter 08 server side

--- src/server/render-app.jsx
+++ src/server/render-app.jsx
@@ -3,21 +3,28 @@
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import Helmet from 'react-helmet'
+import { SheetsRegistry, JssProvider } from 'react-jss'
+
 import { Provider } from 'react-redux'
 import { StaticRouter } from 'react-router'
 
 import initStore from './init-store'
 import App from '../shared/app'
-import { APP_CONTAINER_CLASS, STATIC_PATH, WDS_PORT } from '../shared/config'
+import {
+  APP_CONTAINER_CLASS, JSS_SSR_CLASS, STATIC_PATH, WDS_PORT,
+} from '../shared/config'
 import { isProd } from '../shared/utils'
 
 const renderApp = (location: string, plainPartialState: ?Object, routerContext: ?Object = {}) => {
   const store = initStore(plainPartialState)
+  const sheets = new SheetsRegistry()
   const appHtml = ReactDOMServer.renderToString(
     // eslint-disable-next-line react/jsx-filename-extension
     <Provider store={store}>
       <StaticRouter location={location} context={routerContext}>
-        <App />
+        <JssProvider registry={sheets}>
+          <App />
+        </JssProvider>
       </StaticRouter>
     </Provider>,
   )
@@ -30,6 +37,7 @@ const renderApp = (location: string, plainPartialState: ?Object, routerContext: 
          ${head.title}
          ${head.meta}
          <link rel="stylesheet" href="${STATIC_PATH}/css/bootstrap.min.css">
+         <style class="${JSS_SSR_CLASS}">${sheets.toString()}</style>
        </head>
        <body>
          <div class="${APP_CONTAINER_CLASS}">${appHtml}</div>
